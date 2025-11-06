c6c300da721d5ee87f7edf85935e4eb141776128
Abhrajit Gupta <abhrajitgupta.it2022@nsec.ac.in>
2025-11-06 18:00:52 +0530

    fixed mongodb issue

--- backend/connectDb.js
+++ backend/connectDb.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
-const connectToMongo = async ()=>{
-    try{
-        await mongoose.connect(`${process.env.MONGO_URI}`);
+let isConnected = false;
+
+const connectToMongo = async () => {
+
+    if (isConnected) {
+        console.log("Mongo already connected.");
+        return;
+    }
+
+    try {
+        const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
+        isConnected = conn.connections[0].readyState;
         console.log('Connected to MongoDB Successfully');
     }
-    catch(error){
+    catch (error) {
         console.error('Error connecting to MongoDB', error);
     }
 }
